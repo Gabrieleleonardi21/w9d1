b9795896b9b57ffdf9cd66e94388425b1d9c30ee
Gabriele <leonardi.gabriele9821@gmailo.com>
2026-06-29 16:56:33 +0200

    fatto con la classe

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,17 @@
 // Card immagine riutilizzabile.
 // Riceve via props la sorgente dell'immagine (src) e il testo alternativo (alt).
-function Card({ src, alt }) {
-  return (
-    <div className="card">
-      <img src={src} alt={alt} className="card-img" />
-    </div>
-  )
+import { Component } from 'react'
+
+class Card extends Component {
+  render() {
+    // Estraggo le props dall'istanza della classe.
+    const { src, alt } = this.props
+    return (
+      <div className="card">
+        <img src={src} alt={alt} className="card-img" />
+      </div>
+    )
+  }
 }
 
 export default Card
